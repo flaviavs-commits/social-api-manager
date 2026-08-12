e0fb25fc43532ceda4d011db25b109221e19a5d9
Social Api Manager <flaviavs@vitissouls.com>
2026-08-12 10:20:44 -0300

    build: atualizar interface de conexao do Facebook

---
+++ public/react/assets/sched-section-dYQvdWX3.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-IOSPpan7.js";function i({number:e,title:c,children:n}){return s.jsxs("fieldset",{className:"sched-section",children:[s.jsxs("legend",{className:"sched-section-title",children:[s.jsx("span",{className:"sched-section-num",children:e})," ",c]}),n]})}export{i as S};
